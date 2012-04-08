083d619028e781a70847c834fcfeef4ed380edc3
igorcb <igor.batista@gmail.com>
2012-04-08 16:54:37 -0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleAppRails3::Application.routes.draw do
+  match '/signup',  to: 'users#new'
+          
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
