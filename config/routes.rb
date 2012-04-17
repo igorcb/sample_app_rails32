f319e243be3c9b76326df7b8bf89390882fc613e
Igor Batista <igor.batista@gmail.com>
2012-04-17 12:40:11 -0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 SampleAppRails3::Application.routes.draw do
 
-#  match '/signup',  to: 'users#new'
   resources :users
           
+  root to: 'static_pages#home'
 
+  match '/signup',  to: 'users#new'
+#  match '/signin',  to: 'sessions#new'
+  #match '/signout', to: 'sessions#destroy', via: :delete
+      
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-
-  root to: 'static_pages#home'
-
-#  get "static_pages/home"
-#  get "static_pages/help"
-#  get "static_pages/about"
-#  get "static_pages/contact"
 
 
   # The priority is based upon order of creation:
